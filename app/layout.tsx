9e9956da1e55b7997a772519eaa58ca79a006d3c
Bani <banikur2103@gmail.com>
2026-05-25 11:33:21 +0700

    feat: initialize root layout with error boundary and update project metadata title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
-import type {Metadata} from 'next';
+import type { Metadata } from 'next';
 import './globals.css'; // Global styles
 import ErrorBoundary from './ErrorBoundary';
 
 export const metadata: Metadata = {
-  title: 'Materi Alur Pengadaan',
+  title: 'Mockup Proses Pengadaan',
   description: 'Pengenalan interaktif alur pengadaan barang/jasa militer',
 };
 
-export default function RootLayout({children}: {children: React.ReactNode}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body suppressHydrationWarning>
